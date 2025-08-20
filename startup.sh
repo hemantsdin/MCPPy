d4f2602c57c1c328973b5cb45e9d826a74f0d477
Hemant SDIN <hemant_sdin@hotmail.com>
2025-08-20 21:33:14 +0530

    port change

--- startup.sh
+++ startup.sh
@@ -4,5 +4,6 @@
 # source myenv/bin/activate
 
 # Start the MCP server
-mcp dev sample.py --host 0.0.0.0 --port ${PORT}
+mcp dev sample.py --host 0.0.0.0 --port 3000
+#mcp dev sample.py --host 0.0.0.0 --port ${PORT}
 
